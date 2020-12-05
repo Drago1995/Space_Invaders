7778b9a156931c0b0f327d2f2eb6c9228fab8bba
Gilberto <drago19.07.95@gmail.com>
2020-12-05 16:12:55 -0600

    Se definen los aliens en un array

---
+++ app.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll('.grid div')
+    const resultDisplat = document - querySelectorAll('#result')
+
+    let width = 15
+    let currentShooterIndex = 202
+    let currentInvaderIndex = 0
+    let alienInvaderTakenDown = []
+    let result = 0
+    let direction = 1
+    let invaderID
+
+    //Denfine los invasores alienigenas
+    const alienInvaders = [
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+        15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+        30, 31, 32, 33, 34, 35, 36, 37, 38, 39
+    ]
+
+    //
+
+})
