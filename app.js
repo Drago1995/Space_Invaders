a95f3025312403ed8962aa6da086e40c172676dc
Gilberto <drago19.07.95@gmail.com>
2020-12-05 17:53:04 -0600

    Mover invasores

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const squares = document.querySelectorAll('.grid div')
-    const resultDisplat = document.querySelectorAll('#result')
+    const resultDisplay = document.querySelectorAll('#result')
 
     let width = 15
     let currentShooterIndex = 202
@@ -40,8 +40,43 @@ document.addEventListener('DOMContentLoaded', () => {
     document.addEventListener('keydown', moveShooter)
 
 
+    // mover invasores
+
+    function moveInvaders() {
+        const leftEdge = alienInvaders[0] % width === 0
+        const rightEdge = [alienInvaders.length - 1] % width === width - 1
+        if ((leftEdge && direction === -1) || (rightEdge && direction === 1)) {
+            direction = width
+        } else if (direction === width) {
+            if (leftEdge) direction = 1
+            else direction = -1
+        }
+        for (let i = 0; i <= alienInvaders.length - 1; i++) {
+            squares[alienInvaders[i]].classList.remove('invader')
+        }
+        for (let i = 0; i <= alienInvaders.length - 1; i++) {
+            alienInvaders[i] += direction
+        }
+        for (let i = 0; i <= alienInvaders.length - 1; i++) {
+            squares[alienInvaders[i]].classList.add('invader')
+        }
 
 
+        //Decición de ganador
+        if (squares[currentShooterIndex].classList.contains('invader', 'shooter')) {
+            resultDisplay.textContent = 'Game Over'
+            squares[currentShooterIndex].classList.add('boom')
+            clearInterval(invaderID)
+        }
+
+        for (let i = 0; i <= alienInvaders.length - 1; i++) {
+            if (alienInvaders[i] > (squares.length(width - 1))) {
+                resultDisplay.textContent = 'Game Over'
+                clearInterval(invaderID)
+            }
+        }
+    }
+    invaderID = setInterval(moveInvaders, 500)
 
 
 
